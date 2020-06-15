c41d507ee863f0f6a32d3491ce7786544e152732
Dorian Machado <dorian599@gmail.com>
2020-06-15 16:47:37 -0300

    changes function current_user_id

--- 3__z_log__function_current_user_id.sql
+++ 3__z_log__function_current_user_id.sql
@@ -1,4 +1,5 @@
-CREATE FUNCTION z_logs.current_user_id()
+CREATE OR REPLACE FUNCTION z_logs.current_user_id()
 RETURNS TEXT AS $$
-  SELECT authentication.current_user_id()::TEXT
+  --SELECT authentication.current_user_id()::TEXT
+  SELECT 'noUserId'::TEXT;
 $$ LANGUAGE SQL STABLE;
